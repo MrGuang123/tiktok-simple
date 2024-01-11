3950106774e487ea21db4fd89a6ecdf85d3b838f
ytg <522125842@qq.com>
2024-01-11 10:59:25 +0800

    chore: add graphql and enabled cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,39 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cookieParser from 'cookie-parser'
+import { BadRequestException, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: 'http://127.0.0.1:5173',
+    credentials: true,
+    allowedHeaders: [
+      'Accept',
+      'Authorization',
+      'Content-Type',
+      'X-Requested-With',
+      'apollo-require-preflight'
+    ],
+    methods: ['GET', 'PUT', 'POST', 'DELETE', 'OPTIONS']
+  })
+
+  app.use(cookieParser())
+
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+    transform: true,
+    exceptionFactory: (errors) => {
+      const formattedErrors = errors.reduce((all, error) => {
+        all[error.property] = Object.values(error.constraints).join(',')
+        return all
+      })
+
+      throw new BadRequestException(formattedErrors)
+    }
+  }))
+
   await app.listen(3000);
 }
 bootstrap();
