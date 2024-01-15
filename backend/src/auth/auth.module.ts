f818fb36726b3bd89fac2ea928be22444686966a
ytg <522125842@qq.com>
2024-01-15 15:11:54 +0800

    feat: add graphql...

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,7 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { JwtModule, JwtService } from '@nestjs/jwt';
+import { jwtConstants } from './jwt.strategy/constants';
+import { PrismaService } from 'src/prisma.service';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
-  providers: [AuthService]
+  imports: [
+    JwtModule.register({
+      secret: jwtConstants.secret,
+      global: true
+    })
+  ],
+  providers: [
+    AuthService,
+    PrismaService,
+    ConfigService,
+    JwtService
+  ],
+  exports: [
+    JwtService,
+    AuthService
+  ]
 })
-export class AuthModule {}
+export class AuthModule { }
